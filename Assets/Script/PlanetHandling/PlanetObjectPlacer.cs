77c28fb78e3ef835e970e1f99002da57b8502cb3
Erik Kubiak <erikkubiak@gmail.com>
2022-04-23 10:17:44 +0200

    Finished Planet base

---
+++ Assets/Script/PlanetHandling/PlanetObjectPlacer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlanetHandling
+{
+    public class PlanetObjectPlacer : MonoBehaviour
+    {
+        [SerializeField] private Vector2 m_Coordinate;
+        [SerializeField] private SphereCollider m_Collider;
+
+        private void OnValidate()
+        {
+            UpdatePosition();
+        }
+
+        private void UpdatePosition()
+        {
+            if (m_Collider == null)
+            {
+                return;
+            }
+
+            float radius = m_Collider.transform.lossyScale.x * m_Collider.radius;
+            Vector3 newPosition = new Vector3(
+                Mathf.Sin(m_Coordinate.x) * Mathf.Cos(m_Coordinate.y),
+                Mathf.Cos(m_Coordinate.x),
+                Mathf.Sin(m_Coordinate.x) * Mathf.Sin(m_Coordinate.y)
+            ) * radius;
+            transform.position = newPosition;
+            transform.up = newPosition - m_Collider.center;
+        }
+        
+        
+    }
+}
---
+++ Assets/Script/PlanetHandling/PlanetTool.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlanetHandling
+{
+    public class PlanetTool : MonoBehaviour
+    {
+        [Header("Settings")]
+        [Range(10, 170)]
+        [SerializeField] private float m_Orientation;
+        [SerializeField] private Vector3 m_Axis = new Vector3(0, -1, 0);
+        
+        [Header("Objects")]
+        [SerializeField] private Transform[] m_ObjectToRotate;
+        
+
+        private void OnValidate()
+        {
+            UpdateObjects();
+        }
+
+        private void UpdateObjects()
+        {
+            foreach (var t in m_ObjectToRotate)
+            {
+                if (t == null)
+                {
+                    continue;
+                }
+                t.localRotation = Quaternion.Euler(m_Axis * m_Orientation);
+            }
+        }
+    }
+}
