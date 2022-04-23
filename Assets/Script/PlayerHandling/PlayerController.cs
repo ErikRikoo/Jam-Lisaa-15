5f1580c4bfc1348c30184c7fdbbde5a514f08046
Erik Kubiak <erikkubiak@gmail.com>
2022-04-23 14:24:50 +0200

    Adding inputs

--- Assets/Script/PlayerHandling/PlayerController.cs
+++ Assets/Script/PlayerHandling/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 
 namespace Script.PlayerHandling
@@ -11,6 +12,8 @@ namespace Script.PlayerHandling
 
         private Rigidbody m_RigidBody;
 
+        private bool m_IsGrounded = true;
+
         private void Awake()
         {
             m_RigidBody = GetComponent<Rigidbody>();
@@ -18,7 +21,20 @@ namespace Script.PlayerHandling
 
         public void Jump()
         {
+            if (!m_IsGrounded)
+            {
+                return;
+            }
             m_RigidBody.AddForce(transform.up * m_JumpStrength);
+            m_IsGrounded = false;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.contacts.Any(contactPoint => contactPoint.normal == transform.up))
+            {
+                m_IsGrounded = true;
+            }
         }
 
         public void Crouch()
