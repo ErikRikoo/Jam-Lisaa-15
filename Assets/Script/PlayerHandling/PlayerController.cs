378cf68407e9019eb6e48def75d73db3dec8e3e7
Erik Kubiak <erikkubiak@gmail.com>
2022-04-23 13:36:59 +0200

    Add URP

---
+++ Assets/Script/PlayerHandling/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Script.PlayerHandling
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerController : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private float m_JumpStrength;
+
+        private Rigidbody m_RigidBody;
+
+        private void Awake()
+        {
+            m_RigidBody = GetComponent<Rigidbody>();
+        }
+
+        public void Jump()
+        {
+            m_RigidBody.AddForce(transform.up * m_JumpStrength);
+        }
+
+        public void Crouch()
+        {
+            
+        }
+    }
+}
